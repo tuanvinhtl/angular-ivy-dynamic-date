d90aa1609f7e86669c4617456527f561c57af20d
tuanvinhtl <tuanvinhtl@gmail.com>
2022-07-03 11:21:52 +0700

    2 files modified

--- src/app/overlay-slot.directive.ts
+++ src/app/overlay-slot.directive.ts
@@ -2,14 +2,19 @@ import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
 
 @Directive({ selector: '[appRender]' })
 export class OverlaySlotDirective {
-  @Input() set appUnless(condition) {}
+  @Input() set appUnless(condition) {
+    // let div = document.createElement('div');
+    // div.className = 'overlay-slot';
+    // this.templateRef.elementRef.nativeElement.appendChild(div);
+    this.viewContainer.createEmbeddedView(this.templateRef);
+  }
   constructor(
     private templateRef: TemplateRef<any>,
     private viewContainer: ViewContainerRef
   ) {
-    let div = document.createElement('div');
-    div.className = 'overlay-slot';
-    this.templateRef.elementRef.nativeElement.appendChild(div);
-    this.viewContainer.createEmbeddedView(this.templateRef);
+    // let div = document.createElement('div');
+    // div.className = 'overlay-slot';
+    // this.templateRef.elementRef.nativeElement.appendChild(div);
+    // this.viewContainer.createEmbeddedView(null);
   }
 }
